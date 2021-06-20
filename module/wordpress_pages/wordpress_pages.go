1192e2e53ab1b2ad7be027f4908d1bdb9ec86a5f
Thorsten Klöhn <thorstenkloehn@gmail.com>
2021-06-20 17:59:16 +0200

    Start

--- module/wordpress_pages/wordpress_pages.go
+++ module/wordpress_pages/wordpress_pages.go
@@ -1,13 +1,19 @@
 package wordpress_pages
 
 import (
+	"encoding/json"
 	"fmt"
+	"github.com/thorstenkloehn/ahrensburg.digital/models"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
+	"io/ioutil"
 	"log"
+	"net/http"
+	"os"
+	"text/template"
 )
 
-type Bloginhalt struct {
+type BlogInhalt struct {
 	Id      int    `json:"id"`
 	Date    string `json:"date"`
 	DateGmt string `json:"date_gmt"`
@@ -88,11 +94,61 @@ type Bloginhalt struct {
 	} `json:"_links"`
 }
 
+type BlogSite struct {
+	Inhalt []BlogInhalt
+	Titel  string
+	Urls   string
+}
+
+type Blogausgabe struct {
+	BlogSite    []BlogSite
+	SeitenTitel string
+}
+
 func Start() {
-	db, err := gorm.Open(sqlite.Open("Datenbank.db"), &gorm.Config{})
+	db, err := gorm.Open(sqlite.Open("datenbank.db"), &gorm.Config{})
 	if err != nil {
 
 		log.Fatal(err)
 	}
+
+	ausgaben := []models.Wordpress{}
+	db.Find(&ausgaben)
+	fmt.Println(ausgaben)
+	var vorlagen, _ = template.ParseGlob("views/*")
+
+	blogausgabe := []Blogausgabe{}
+	for _, ausgabe := range ausgaben {
+		blogausgabe1 := Blogausgabe{BlogSite: []BlogSite{{Urls: ausgabe.Url, Titel: ausgabe.Titel}}}
+		blogausgabe = append(blogausgabe, blogausgabe1)
+	}
+	for _, ausgabe := range blogausgabe {
+		ausgabe.BlogSite[0].Lesen()
+	}
+	f, err := os.Create("output/index.html")
+	if err != nil {
+		fmt.Println(err)
+		f.Close()
+		return
+	}
+	vorlagen.ExecuteTemplate(f, "wordpresspagesSeite.html", &blogausgabe)
+	f.Close()
+	fmt.Println("Seite bearbeitet")
+
+}
+
+func (blog *BlogSite) Lesen() *BlogSite {
+
+	url := blog.Urls + "/wp-json/wp/v2/posts"
+
+	req, _ := http.NewRequest("GET", url, nil)
+
+	res, _ := http.DefaultClient.Do(req)
+
+	defer res.Body.Close()
+
+	body1, _ := ioutil.ReadAll(res.Body)
+	json.Unmarshal((body1), &blog.Inhalt)
+	return blog
 
 }
